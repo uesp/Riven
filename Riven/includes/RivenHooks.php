289739072f9f90c7d226758793973af084db8b54
Robin Hood <robinhood70@live.ca>
2023-03-08 01:12:36 -0500

    Standard call does synonyms

--- Riven/includes/RivenHooks.php
+++ Riven/includes/RivenHooks.php
@@ -111,7 +111,7 @@ class RivenHooks /* implements
 	 */
 	private static function initTagFunctions(Parser $parser): void
 	{
-		ParserHelper::setHookSynonyms($parser, self::TG_CLEANSPACE, 'Riven::doCleanSpace');
-		ParserHelper::setHookSynonyms($parser, self::TG_CLEANTABLE, 'Riven::doCleanTable');
+		$parser->setHook(self::TG_CLEANSPACE, 'Riven::doCleanSpace');
+		$parser->setHook(self::TG_CLEANTABLE, 'Riven::doCleanTable');
 	}
 }
